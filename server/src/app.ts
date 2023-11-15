6a5b5eeaddb99c53b367d06f53e49f7da3ba485b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-15 14:06:00 +0800

    remove unused route

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,9 +12,6 @@ const app: Application = express()
 app.use(express.json());
 app.use("/todo", todoRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express & TypeScript Server');
-});
 app.use((res, req, next) => {
     next(createHttpError(404, "Endpoint not found"));
 });
