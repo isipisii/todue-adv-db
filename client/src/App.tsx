b2b5007778649a88e3bd53376b40630ad9c7716a
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:00:42 +0800

    remove providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,14 @@
-import { ThemeProvider } from './components/providers/theme-provider';
-import { TodoProvider } from './components/providers/todo-provider';
 import { ModeToggle } from './components/mode-toggle';
+import EditTodoDialog from './components/edit-todo-dialog';
 import Todos from './components/todos';
 
 function App() {
   return (
-    <ThemeProvider>
-      <TodoProvider>
-        <main className='flex items-center justify-center w-full'>
-          <ModeToggle />
-          <Todos />
-        </main>
-      </TodoProvider>
-    </ThemeProvider>
+    <main className='flex items-center justify-center w-full'>
+      <EditTodoDialog />
+      <ModeToggle />
+      <Todos />
+    </main>
   )
 }
 
