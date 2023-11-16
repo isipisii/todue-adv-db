783f182982e899a471a012a26eeec17528009992
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:24:04 +0800

    add todo dropdown

--- client/src/components/todo.tsx
+++ client/src/components/todo.tsx
@@ -6,19 +6,22 @@ import {
 } from "../components/ui/card"
 import { Checkbox } from "./ui/checkbox"
 import { LayoutList } from 'lucide-react';
+import TodoDropdown from "./todo-dropdown";
 
-function Todo() {
-
+function Todo({ todo }: {todo: TTodo}) {
   return (
     <Card className="w-full flex items-center justify-between p-4">
         <div className="flex items-center gap-4">
             <LayoutList className="text-[1.5rem]" />
             <CardHeader className="p-0">
-                <CardTitle className="text-lg">Study</CardTitle>
-                <CardDescription className="text-xs">Card Description</CardDescription>
+                <CardTitle className="text-lg">{todo.title}</CardTitle>
+                <CardDescription className="text-xs">{todo.description}</CardDescription>
             </CardHeader>
         </div>
-        <Checkbox/>
+        <div className="flex flex-col gap-4 items-center">
+          <TodoDropdown todo={todo}/>
+          <Checkbox/>
+        </div>
     </Card>
   )
 }
