951b003e47de2634b1510dd9bb210162ba85478b
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:25:47 +0800

    add providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TodoProvider } from './components/providers/todo-provider.tsx'
+import { ThemeProvider } from './components/providers/theme-provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
