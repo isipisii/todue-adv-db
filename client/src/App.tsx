5e633f64625bdade9a9553709fb93d90acce545f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 19:33:48 +0800

    add todo percentage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import { ModeToggle } from './components/mode-toggle';
 import EditTodoDialog from './components/edit-todo-dialog';
 import Todos from './components/todos';
+import TodoPercentage from './components/todo-percentage';
+import { useTodo } from './components/providers/todo-provider';
 
 function App() {
+  const { todos } = useTodo()
   return (
-    <main className='flex items-center justify-center w-full'>
+    <main className='flex items-center justify-center w-full h-screen gap-4 md:flex-row flex-col'>
       <EditTodoDialog />
       <ModeToggle />
-      <Todos />
+      <Todos /> 
+      {todos.length > 0 && <TodoPercentage />}
     </main>
   )
 }
