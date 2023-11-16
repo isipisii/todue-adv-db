dac16f355020adb7466923fb16b998dca9b000fb
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:21:53 +0800

    create to-do form for editing and creating

---
+++ client/src/components/todo-form.tsx
@@ -0,0 +1,108 @@
+import { useForm, } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import z from "zod";
+
+import { Button } from "./ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
+import { Input } from "./ui/input"
+
+import { useTodo } from "./providers/todo-provider";
+import { createTodo, updateTodo } from "@/services/todoApi";
+
+const todoFormSchema = z.object({
+    title: z.string({
+        required_error: "To-do title is required",
+        invalid_type_error: "Name must be a string",
+    }).min(6, {
+        message: "To-do title must be at least 6 characters"
+    }).max(20, {
+        message: "To-do title should not exceed to 20 characters"
+    }),
+    description: z.string().max(100, {
+        message: "To-do description should not exceed to 100 characters"
+    }),
+})
+
+export type TTodoForm = z.infer<typeof todoFormSchema>
+
+function TodoForm({ isEditing, setOpen }: { isEditing?: boolean, setOpen: (atate: boolean) => void }) {
+    const { todos, setTodos, todoToEdit, setTodoToEdit } = useTodo()
+    const form = useForm<TTodoForm>({
+        resolver: zodResolver(todoFormSchema),
+        defaultValues: {
+            title: isEditing ? todoToEdit?.title : "",
+            description: isEditing ? todoToEdit?.description : ""
+        },
+    })
+    
+    async function onSubmitCreateTodo(values: TTodoForm) {
+        try {
+            const newTodo = await createTodo(values)
+            setTodos([...todos, newTodo])
+
+            //this will close the specific dialog from where its mounted
+            setOpen(false)
+        } catch (error) {
+            console.error(error)
+        }
+    }    
+
+    async function onSubmitEditTodo(values: TTodoForm) {
+        try {
+            if(!todoToEdit) return
+
+            const updatedTodo = await updateTodo(values, todoToEdit._id)
+            const updatedTodoArr = todos.map(todo => todo._id === updatedTodo._id ? {...updatedTodo} : todo) as TTodo[]
+            
+            setTodos(updatedTodoArr)
+             //this will close the specific dialog from where its mounted
+            setTodoToEdit(null)
+            setOpen(false)
+        } catch (error) {
+            console.error(error)
+        }
+    }    
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(isEditing ? onSubmitEditTodo : onSubmitCreateTodo)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Put your to-do title" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                 <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Put your to-do description" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="w-full">Add</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default TodoForm
