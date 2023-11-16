737c4533657b86bf792a495e0a0bcda563614926
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:04:27 +0800

    create to-do dropdown

---
+++ client/src/components/todo-dropdown.tsx
@@ -0,0 +1,49 @@
+import { MoreHorizontal, Trash2, Pencil} from 'lucide-react';
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useTodo } from './providers/todo-provider';
+import { deleteTodo } from '@/services/todoApi';
+
+export default function TodoDropdown({ todo }: {todo: TTodo}) {
+    const { setOpenEditTodoDialog, setTodoToEdit, todos, setTodos } = useTodo()
+    
+    async function handleDeleteTodo(todoId: string) {
+        try {
+            const response = await deleteTodo(todoId)
+            console.log(response)
+            const updatedTodoArr = todos.filter(todo => todo._id !== todoId)
+
+            setTodos(updatedTodoArr)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild className='p-0'>
+            <Button variant="link" size="sm" className='h-4'>
+                <MoreHorizontal className='text-xs'/>
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => {
+                setTodoToEdit(todo)
+                setOpenEditTodoDialog(true)
+            }}>
+                <Pencil className="w-4 h-4 mr-3" />
+                Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem className='!text-red-500' onClick={() => handleDeleteTodo(todo._id)}>
+                <Trash2 className="w-4 h-4 mr-3" />
+                Delete
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
