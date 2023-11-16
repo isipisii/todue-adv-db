b1df010c691710ec22260a25411921b94c31912f
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-16 08:24:34 +0800

    map to-dos from api

--- client/src/components/todos.tsx
+++ client/src/components/todos.tsx
@@ -1,17 +1,19 @@
 import Todo from "./todo"
 import { useTodo } from "./providers/todo-provider"
+import CreateTodoDialog from "./create-todo-dialog"
 
 function Todos() {
-    const { todos } = useTodo()
-
-    console.log(todos)
+  const { todos } = useTodo()
   return (
     <div className="flex flex-col gap-4 rounded-lg p-4 w-full max-w-[600px]">
-        <h2 className="text-2xl font-medium">Todue</h2>
+        <div className="flex justify-between items-center w-full">
+          <h2 className="text-2xl font-medium">Todue</h2>
+          <CreateTodoDialog />
+        </div>
         <div className="overflow-y-auto max-h-[400px]">
             <div className="flex gap-2 flex-col">
-                {[... new Array(10)].map((_, index) => (
-                    <Todo key={index} />
+                {todos?.map((todo, index) => (
+                    <Todo key={index} todo={todo} />
                 ))}
             </div>
         </div>
