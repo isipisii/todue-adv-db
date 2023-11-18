7c1c34be6b1ff2a2bfc90312b6b51206487ec166
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-11-18 10:36:23 +0800

    add toaster component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,18 @@ import EditTodoDialog from './components/edit-todo-dialog';
 import Todos from './components/todos';
 import TodoPercentage from './components/todo-percentage';
 import { useTodo } from './components/providers/todo-provider';
+import { Toaster } from 'sonner';
 
 function App() {
   const { todos } = useTodo()
+
   return (
     <main className='flex items-center justify-center w-full h-screen gap-4 md:flex-row flex-col'>
+      <Toaster 
+        richColors 
+        position="bottom-right"
+        expand={true}
+      />
       <EditTodoDialog />
       <ModeToggle />
       <Todos /> 
